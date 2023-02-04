fb7415a5feb9530f138e6cc23d9e09e80b88b37b
dudddy <zernin.egor2077@yandex.ru>
2023-02-04 19:42:21 +0300

    first commit

---
+++ weather.py
@@ -0,0 +1,13 @@
+from coordinates import get_gps_coordinates
+from weather_api_service import get_weather
+from weather_formatter import format_weather
+
+
+def main():
+    coordinates = get_gps_coordinates()
+    weather = get_weather(coordinates)
+    print(format_weather(weather))
+
+
+if __name__ == '__main__':
+    main()
